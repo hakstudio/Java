20485ec69ba541ecc8a868df44121abb1b762ad0
HAKStudio <53950869+HAKStudio@users.noreply.github.com>
2021-12-21 00:35:16 +0300

    Update TekrariSil.java

--- TekrariSil.java
+++ TekrariSil.java
@@ -10,13 +10,14 @@ public class TekrariSil {
         System.out.println(arrayList);
         System.out.println(tekrariSil(arrayList));
     }
-    static ArrayList<String> tekrariSil(ArrayList<String> arrayList){
-        for (int i=0; i<arrayList.size(); i++){
-            String anaEleman=arrayList.get(i);
-            for (int j=i+1; j< arrayList.size(); j++){
-                String eleman=arrayList.get(j);
-                if(anaEleman.equals(eleman)){
+    static ArrayList<String> tekrariSil(ArrayList<String> arrayList) {
+        for (int i = 0; i < arrayList.size(); i++) {
+            String anaEleman = arrayList.get(i);
+            for (int j = i+1; j < arrayList.size(); j++) {
+                String eleman = arrayList.get(j);
+                if (anaEleman.equals(eleman)) {
                     arrayList.remove(j);
+                    j--;
                 }
             }
         }
